d6b577c99873b4bfef43403ac3b557dbf605558d
saleh-programs <muradsaleh2022@gmail.com>
2025-12-29 12:44:14 -0500

    Added Animation components from different project

---
+++ src/assets/Animation.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useRef, useState } from "react"
+import {preload} from "./preloadedImages.js"
+
+function Animation({path, type="loop", speed=1, onClick=null}){
+    const frame = useRef(0)
+    const frameList = useRef([])
+    const canvasRef = useRef(null)
+    const cxtRef = useRef(null)
+
+    const rafRef = useRef(null)
+
+    const [dimensions, setDimensions] = useState(null)
+
+    useEffect(()=>{
+        const [frames, loadingFrames] = preload(path)
+        loadingFrames.then(()=>{
+            setDimensions([frames[0].width, frames[0].height])
+            frameList.current = frames
+        })
+    },[path])
+
+    useEffect(()=>{
+        if (!dimensions){
+            return
+        }
+
+        cxtRef.current = canvasRef.current.getContext("2d")
+        frame.current = 0;
+
+        let last = Date.now()
+        const updateCanvas = () => {
+            cxtRef.current.clearRect(0,0,dimensions[0],dimensions[1])
+            cxtRef.current.drawImage(frameList.current[Math.floor(frame.current)], 0, 0)
+            const currTime = Date.now()
+            frame.current += ((currTime - last) / 1000) * speed
+            last = currTime
+
+            if (frame.current >= frameList.current.length){
+                frame.current = 0
+                if (type !== "loop"){ 
+                    return
+                }
+            }
+            rafRef.current = requestAnimationFrame(updateCanvas)
+        }
+
+        if (type !== "button"){
+            updateCanvas()
+        }else{
+            cxtRef.current.clearRect(0,0,dimensions[0],dimensions[1])
+            cxtRef.current.drawImage(frameList.current[Math.floor(frame.current)], 0, 0)
+        }
+        return () => {
+            cancelAnimationFrame(rafRef.current)
+        }
+    }, [dimensions])
+
+    function replay(){
+        if (!onClick){
+            return
+        }
+        cancelAnimationFrame(rafRef.current)
+        onClick()
+        frame.current = 1
+        
+        let last = Date.now()
+        const updateCanvas = () => {
+            cxtRef.current.clearRect(0,0,dimensions[0],dimensions[1])
+            cxtRef.current.drawImage(frameList.current[Math.floor(frame.current)], 0, 0)
+            const currTime = Date.now()
+            frame.current += ((currTime - last) / 1000) * speed
+            last = currTime
+
+            if (frame.current >= frameList.current.length){
+                frame.current = 0
+                rafRef.current = requestAnimationFrame(()=>{
+                    cxtRef.current.clearRect(0,0,dimensions[0],dimensions[1])
+                    cxtRef.current.drawImage(frameList.current[0], 0, 0)
+                })
+                return
+            }
+            rafRef.current = requestAnimationFrame(updateCanvas)
+        }
+        updateCanvas()
+    }
+    
+    if (!dimensions){
+        return <></>
+    }
+    return(
+        <>
+            <canvas ref={canvasRef} width={dimensions[0]} height={dimensions[1]} onClick={replay}/>
+        </>
+    )
+}
+
+export default Animation
---
+++ src/assets/preloadedImages.js
@@ -0,0 +1,52 @@
+
+const animations = {
+    "/willowIntro?23": []
+}
+
+function preloadAll(){
+    const values = Object.entries(animations)
+    const allImages = []
+    for (let i = 0; i < values.length; i++){
+        if (animations[values[i][0]].length > 0){
+            continue
+        }
+        const [folder, numFiles] = values[i][0].split("?")
+        for (let j = 0; j < Number(numFiles); j++){
+            const frame = new Image()
+            frame.src = `${folder}/${j}.png`
+            animations[values[i][0]].push(frame)
+
+            allImages.push(new Promise(resolve=>{
+                frame.onload = resolve
+            }))
+        }
+    }
+    const loadingImages = Promise.all(allImages).then(()=>{
+        console.log("All images loaded! at ", Date.now())
+    })
+    return [animations, loadingImages]
+}
+
+function preload(path){
+    if (animations[path].length > 0){
+        return [animations[path], Promise.resolve()]
+    }
+    const [folder, numFiles] = path.split("?")
+    const allImages = []
+
+    for (let i = 0; i < Number(numFiles); i++){
+        const frame = new Image()
+        frame.src = `${folder}/${i}.png`
+        animations[path].push(frame)
+
+        allImages.push(new Promise(resolve=>{
+            frame.onload = resolve
+        }))
+    }
+    const loadingImages = Promise.all(allImages).then(()=>{
+        console.log("All images loaded! at ", Date.now())
+    })
+    return [animations[path], loadingImages]
+}
+
+export {preload, preloadAll}
