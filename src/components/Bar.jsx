ebcc28be9a9d2c9cfa69a6a3b4f1695adabe5b79
saleh-programs <muradsaleh2022@gmail.com>
2026-09-04 20:03:54 -0400

    add conditional renders

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -6,6 +6,8 @@ import Dropdown from "../assets/images/dropdown.png"
 
 function Bar({name, subname, subname2, contentID, date, link}){
   const [isExpanded,setIsExpanded] = useState(false);
+  const [keepStored, setKeepStored] = useState(false);
+  const keepStoredTimerRef = useRef(false);
   const [contentMaxHeight, setContentMaxHeight] = useState("0");
   const elem = useRef(null);
 
@@ -29,12 +31,25 @@ function Bar({name, subname, subname2, contentID, date, link}){
     }
   },[isExpanded])
 
+  function toggleBar(){
+    clearTimeout(keepStoredTimerRef.current);
+    if (isExpanded){
+      setIsExpanded(false);
+      keepStoredTimerRef.current = setTimeout(() => {
+        setKeepStored(false);
+      }, 5000);
+    }else{
+      setIsExpanded(true);
+      setKeepStored(true);
+    }
+  }
+
   return(
       <div className={styles['dropdown-ref']}>
         <div className={`${styles.dropdown} ${isExpanded ? styles.rotateDropdown:""}`} onClick={()=>setIsExpanded(prev => !prev)}>
           <img src={Dropdown} alt="" />
         </div>
-        <div className={styles.bar} onClick={()=>setIsExpanded(prev => !prev)}>
+        <div className={styles.bar} onClick={toggleBar}>
           <div className={styles.barHeader}>
             <div className={styles.barLeft}>
               {name}
@@ -65,7 +80,7 @@ function Bar({name, subname, subname2, contentID, date, link}){
             }
           </div>
           <div className={styles.contentContainer} style={{maxHeight: contentMaxHeight}} ref={elem}>
-            {contentDict[contentID]}
+            {keepStored && contentDict[contentID]}
           </div>
         </div>
       </div>
